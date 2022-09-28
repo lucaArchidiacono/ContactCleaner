36939a93955652fd3b13435039761ca94492da0b
Archidiacono Luca 43379114 <luca.archidiacono@ubs.com>
2022-09-28 17:04:29 +0200

    Initial Commit

---
+++ Shared/ContactCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactCleanerApp.swift
+//  Shared
+//
+//  Created by Luca Archidiacono on 28.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ContactCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Luca Archidiacono on 28.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
